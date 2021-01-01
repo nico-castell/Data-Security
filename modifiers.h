129aa880c323a2e1a75aeca6415b7a1a8193f908
nico-castell <74460331+nico-castell@users.noreply.github.com>
2021-01-01 16:36:55 -0300

    First working prototype

---
+++ modifiers.h
@@ -0,0 +1,9 @@
+#pragma once
+
+/// <summary>
+/// Add the numerical values of two characters
+/// </summary>
+/// <param name="base_char">Base character</param>
+/// <param name="key_char">Key character</param>
+/// <returns>An encrypted character</returns>
+char AddChars(const char &base_char, const char &key_char);
