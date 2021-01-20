5833a6fdacbf2a1f120cfa379dafc82f16eb7f6b
nico-castell <74460331+nico-castell@users.noreply.github.com>
2021-01-19 22:19:58 -0300

    Update.

--- Others/toBin.cpp
+++ Others/toBin.cpp
@@ -1,18 +1,46 @@
 #include <iostream>
+#include <unistd.h>
 #include <bitset>
+using std::string;
 
-int main()
+// Only print escape sequence to format output if the output is a console.
+string cprty(const string &formatting)
 {
-    // The input from the user.
-    std::string input;
-    std::cout << "Please input a phrase: ";
-    getline(std::cin, input);
+    if (!isatty(fileno(stdout)))
+        return "";
+    return "\e[" + formatting + "m";
+}
 
-    for (int i = 0; i < input.length(); i++)
+int main(int argc, char *argv[])
+{
+    // Validate arguments.
+    if (argc != 2)
+    {
+        string name = argv[0];
+        std::cerr << "\e[33mERROR: You must input one argument\n"
+                  << "\e[33mUsage:\e[00m " + name + " 'Phrase'\n"
+                  << "\e[36mExample:\e[00m " + name + " 'Hello World!'\n";
+        return 1;
+    }
+
+    // The input from the user.
+    string input = argv[1]; int il = input.length();
+
+    // Print the headers for the table.
+    std::cout << cprty("32") << "Binary  \tDec\tHex\tOct\tChar" << cprty("00") << "\n";
+
+    // Print the table.
+    for (int i = 0; i < il; i++)
     {
         char character = input[i];
         std::bitset<8> binaryForm(character);
-        std::cout << binaryForm << "\t" << int(character) << "\n";
+        // Pretty print everything to the console
+        std::cout << cprty("33") << binaryForm                         // Print the binary.
+                  << cprty("00") << "\t" << (int)character             // Print the decimal.
+                  << cprty("00") << "\t" << std::hex << (int)character // Print the hexadecimal.
+                  << cprty("00") << "\t" << std::oct << (int)character // Print the octal.
+                  << cprty("31") << "\t" << character                  // Print the character.
+                  << cprty("00") << "\n";                              // End the line.
     }
-    // std::cout << "\n";
+    return 0;
 }
