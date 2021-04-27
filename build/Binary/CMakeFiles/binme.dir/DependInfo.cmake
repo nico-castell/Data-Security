7f21717675f48bb2937b0ca8d01a90ec11d16a4d
nico-castell <74460331+nico-castell@users.noreply.github.com>
2021-04-27 18:21:11 -0300

    :sparkles: Add CMake to 3/4 projects

---
+++ build/Binary/CMakeFiles/binme.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/nicolas/Development/AA - Frozen projects/Data Security/Binary/Program.cpp" "Binary/CMakeFiles/binme.dir/Program.cpp.o" "gcc" "Binary/CMakeFiles/binme.dir/Program.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
